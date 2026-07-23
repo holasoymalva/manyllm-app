68da8a62f8234972b612fc45aa1df29858cd3023
Leon Martin <alquimista3301@gmail.com>
2026-07-22 21:34:28 -0600

    Initial Commit

---
+++ many-llms/many_llmsApp.swift
@@ -0,0 +1,17 @@
+//
+//  many_llmsApp.swift
+//  many-llms
+//
+//  Created by Malva on 22/07/26.
+//
+
+import SwiftUI
+
+@main
+struct many_llmsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
